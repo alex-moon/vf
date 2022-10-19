cff9d691786341f016c800fc3f611fb11ec3dab9
Alex Moon <alex@placechangers.co.uk>
2022-10-19 11:33:39 +0100

    VF-1 - start building out styles etc

---
+++ app/src/components/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+  props: {}
+}
+</script>
+
+<style scoped>
+  nav {
+    width: 100%;
+    display: flex;
+    height: 48px;
+  }
+</style>
